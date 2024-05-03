aef5971debd4cf1f449c1cddff4f4b9f1a4914b8
Murod1303 <murodshernazaroff@gmail.com>
2024-05-04 01:12:10 +0500

    Courses section added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import Footer from "./components/footer/Footer";
 import Header from "./components/header/Header";
 import Home from "./pages/Home";
-
+import { motion, useScroll } from "framer-motion";
 function App() {
+  const { scrollYProgress } = useScroll();
   return (
     <main>
+      <motion.div
+        className="progress-bar"
+        style={{ scaleX: scrollYProgress }}
+      />
       <Header />
       <Home />
       <Footer />
